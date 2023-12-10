6185c1b1b19f4bdc159871433489a4c4a28793b1
zynll45 <zeynel.sonmez@hicoders.ch>
2023-12-10 21:41:27 +0100

    The teacherData created and students data edited

---
+++ data/TeachersData.js
@@ -0,0 +1,1 @@
+[{},{}]//buraya ornek teacherslar eklenecek
--- pages/students.js
+++ pages/students.js
@@ -1,41 +1,98 @@
+import { students } from "../data/StudentsData.js";
+
+
+const getStudents =(start,finish)=>{
+  console.log("calisti")
+  let filterStudent =students.slice(start, finish)
+  localStorage.setItem("studentsStart",start);
+  localStorage.setItem("studentsFinish",finish)
+  return filterStudent;
+  }
+  
+const student = getStudents(0,100)
+
+
 export function createStudentPage(){
+
+
+
   const contentDiv = document.getElementById("content");
   contentDiv.innerHTML = `
   <div class="classes ">
- <div class="clasTitle"> <h2>Students</h2></div>
-
- 
- <div class="classBody">
- <div class="classBodyDiv">
- <h2>Fabio yyyyy</h2>
-   <p>Xxxxx weiwej</p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.!</p>
-  <div class="classButton">
-  <button>Average Grade : 7.8</button>
-  
-  </div>
-  </div>
-  
- <div  class="classBodyDiv">
- <h2>Daniel ttttt</h2>
-   <p>Xxxxx weiwej</p>
- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.!</p>
-  <div class="classButton">
-  <button>Average Grade : 8.7</button>
-  
-  </div>
- </div>
-
-
- 
- 
- </div>
- 
- </div>
-  
-  
-  
-  
-  
+  <div class="classesTable"> 
+  <div class="tableDiv">
+    <table class="table">
+      <thead class="thead-dark">
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Name</th>
+          <th scope="col">Teacher</th>
+          <th scope="col">Avarage Not</th>
+        </tr>
+      </thead>
+      <tbody>     
+       </div>
+      </div>
+`;
+let tableContent = `
+    <div class="tableDiv">
+      <table class="table">
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Teacher</th>
+            <th scope="col">Avarage Not</th>
+            <th scope="col">Classes</th>
+            <th scope="col">Process</th>
+          </tr>
+        </thead>
+        <tbody>
   `;
+  student.forEach((student, index) => {
+  tableContent += `
+    <tr>
+      <th scope="row">${index + 1}</th>
+      <td>${student.name}</td>
+      <td>${student.teacher}</td>
+      <td>${student.AvarageNumber}</td>
+      <td>${student.classesName}</td>
+      <td> <button class="ClassesCartButtons">
+      <span class="material-symbols-outlined Button">delete</span>
+    </button>
+    <button class="ClassesCartButtons">
+      <span class="material-symbols-outlined Button">edit_square</span>
+    </button></td>
+    </tr>
+  `;
+});
+
+tableContent += `
+      </tbody>
+    </table>
+    <div class="studentPageButtonsDiv">
+    <button class="studentPageButtons" onclick="prev()"><span class="material-symbols-outlined">
+    arrow_back_ios
+    </span></button>
+    <button class="studentPageButtons" onclick="next()"><span class="material-symbols-outlined">
+    arrow_forward_ios
+    </span></button>
+    </div>
+  </div>  
+`;
+const classesTable = document.querySelector(".classesTable");
+classesTable.innerHTML = tableContent;
 }
+
+window.prev = function(){
+let prev =localStorage.getItem(studentsStart);
+let next =localStorage.getItem(studentsFinish);
+getStudents(prev-5,next-5)
+}
+ 
+window.next= function(){
+  let prev =localStorage.getItem(studentsStart);
+  let next =localStorage.getItem(studentsFinish);
+  getStudents(prev+5,next+5)
+  }
+ 
